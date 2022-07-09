93e5b0b5cc6bd72eb3af1e08c75276818adad8f2
Doan Nguyen <nguyentrongdoan.0@gmail.com>
2022-07-09 11:24:29 -0700

    canbus 3.3V - 5V works

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -43,6 +43,12 @@
 
 /* USER CODE BEGIN PV */
 
+ CAN_RxHeaderTypeDef rxHeader; //CAN Bus Transmit Header
+ CAN_TxHeaderTypeDef txHeader; //CAN Bus Receive Header
+ uint8_t canRX[8] = {0,0,0,0,0,0,0,0};  //CAN Bus Receive Buffer
+ CAN_FilterTypeDef canfil; //CAN Bus Filter
+ uint32_t canMailbox; //CAN Bus Mail box variable
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -88,6 +94,29 @@ int main(void)
   MX_GPIO_Init();
   MX_CAN_Init();
   /* USER CODE BEGIN 2 */
+  canfil.FilterBank = 0;
+	canfil.FilterMode = CAN_FILTERMODE_IDMASK;
+	canfil.FilterFIFOAssignment = CAN_RX_FIFO0;
+	canfil.FilterIdHigh = 0;
+	canfil.FilterIdLow = 0;
+	canfil.FilterMaskIdHigh = 0;
+	canfil.FilterMaskIdLow = 0;
+	canfil.FilterScale = CAN_FILTERSCALE_32BIT;
+	canfil.FilterActivation = ENABLE;
+	canfil.SlaveStartFilterBank = 14;
+
+	// Assign values for transmit header variable
+	txHeader.DLC = 8;
+	txHeader.IDE = CAN_ID_STD;
+	txHeader.RTR = CAN_RTR_DATA;
+	txHeader.StdId = 0x034;
+	txHeader.ExtId = 0x02;
+	txHeader.TransmitGlobalTime = DISABLE;
+
+	// Initiate canbus
+	HAL_CAN_ConfigFilter(&hcan,&canfil);
+	HAL_CAN_Start(&hcan);
+	HAL_CAN_ActivateNotification(&hcan,CAN_IT_RX_FIFO0_MSG_PENDING);
 
   /* USER CODE END 2 */
 
@@ -95,6 +124,11 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  uint8_t csend[] = {0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08};
+	  HAL_CAN_AddTxMessage(&hcan,&txHeader,csend,&canMailbox);
+	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_3);
+	  HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -188,11 +222,22 @@ static void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA3 */
   GPIO_InitStruct.Pin = GPIO_PIN_3;
@@ -204,6 +249,13 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan1)
+{
+	HAL_CAN_GetRxMessage(hcan1, CAN_RX_FIFO0, &rxHeader, canRX);
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_3);
+
+}
 
 /* USER CODE END 4 */
 
---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+- Update clone stlink v2: unplug -> plug it back if the sw cannot detect the device in DFU mode [video](https://www.youtube.com/watch?v=RDrvFFXttAE)
